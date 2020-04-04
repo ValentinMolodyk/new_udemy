f688b58e8988c96406b86c1d0f1a3fe697e379d6
Reenso <valentin.molodyk@gmail.com>
2020-04-04 14:09:51 +0300

    task7 add

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+// data container
+const dataWrpper = document.getElementsByClassName('data')[0];
+//
+
+// values
+const start = document.getElementById('start');
+const budget = document.getElementsByClassName('budget-value')[0];
+const daybudget = document.getElementsByClassName('daybudget-value')[0];
+const level = document.getElementsByClassName('level-value')[0];
+const expenses = document.getElementsByClassName('expenses-value')[0];
+const optionalExpenses = document.getElementsByClassName('optionalexpenses-value')[0];
+const income = document.getElementsByClassName('income-value')[0];
+const monthsavings = document.getElementsByClassName('monthsavings-value')[0];
+const yearsavings = document.getElementsByClassName('yearsavings-value')[0];
+//
+
+// expenses-item
+const expensesOne = document.getElementsByClassName('expenses-item')[0];
+const expensesTwo = document.getElementsByClassName('expenses-item')[1];
+const expensesThree = document.getElementsByClassName('expenses-item')[2];
+const expensesFour = document.getElementsByClassName('expenses-item')[3];
+//
+
+// buttons
+const btnSave = document.getElementsByTagName('button')[1];
+const btnStart = document.getElementsByTagName('button')[2];
+//
+
+//optionalexpenses-item
+const optionalexpenses = document.querySelectorAll('.optionalexpenses-item');
+//all another data
+
+const chooseIncome = document.querySelector('.choose-income');
+const savings = document.querySelector('#savings');
+const chooseSum = document.querySelector('.choose-sum');
+const choosePercent = document.querySelector('.choose-percent');
+const year = document.querySelector('.year-value');
+const month = document.querySelector('.month-value');
+const day = document.querySelector('.day-value');
+
